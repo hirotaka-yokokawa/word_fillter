e4601d2d6c98ed5d736c8c748a36d8f7420dfede
Hirotaka <yokokawa.491992@gmail.com>
2018-11-22 10:28:25 +0900

    NGワードの設定をできるようにした｡出力もできた｡あとは､繰り返すだけ｡

--- word_filter.py
+++ word_filter.py
@@ -8,11 +8,22 @@ class WordFilter:
             text = text.replace(self.my_filter, censored)
             return text
 
-if __name__ == "__main__":
-    my_filter = WordFilter("アーセナル")
 
-    print(my_filter.detect("昨日のアーセナルの試合アツかった！","ピー"))
-    print(my_filter.detect("昨日のリバプールの試合アツかった！","ピー"))
+def ng_word():
+    counter = 1
+
+    while True:
+        NG_word = input("NGワード" + str(counter) + ":")
+        print("NGワードを設定しました｡")
+        counter += 1
+
+        if NG_word == "end":
+            break
+
+        return NG_word
 
 
+my_filter = WordFilter((ng_word()))
 
+print(my_filter.detect("昨日のアーセナルの試合アツかった！", "ピー"))
+print(my_filter.detect("昨日のリバプールの試合アツかった！", "ピー"))
